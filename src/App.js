5e9612ce15372505e912fc1e966e0b068160e30c
Patrícia Silva <patrici.silvaxi@gmail.com>
2021-10-01 14:06:25 -0300

    Carrinho, foi feito botões de add, remove, info, ainda falta algumas coisas, como organizar  os components.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+let productList =[
+  {name: "Camiseta Divertida", price:150, info:"Camiseta de algodão,P"},
+  {name: "Camiseta Lunar", price:100, info:"Camiseta de algodão,M"},
+  {name: "Camiseta Satuno", price:160, info:"Camiseta de algodão,G"}
+]
+
+class Product extends React.Component{
+    constructor (props){
+        super (props);
+        this.state={
+            qty:0
+        };
+        // Eventos
+        this.add = this.add.bind(this);
+        this.remove = this.remove.bind(this);
+        this.showInfo = this.showInfo.bind(this);
+    }
+    add(){
+        this.setState({
+            qty:this.state.qty + 1
+        });
+    }
+    remove(){
+        this.setState({
+        qty:this.state.qty -1
+    });
+    }
+    showInfo(){
+        alert (this.props.info);
+    }
+    render(){
+        return(
+             <div>
+                 <div className="row form-group">
+                     <div className="col-sm-10">
+                         <h4>{this.props.name}: R$ {this.props.price}</h4>
+                     </div>
+                     <div className="col-sm-2 text-right">
+                         Quantidade: {this.state.qty}
+                     </div>
+                     <div className="row btn-toolbar">
+                         <div className="col-6">
+                             <button className="btn btn-outline-primary" onClick={this.showInfo}>Informações</button> 
+                           
+                         </div>
+                         <div className="col-6 text-rigth">
+                         <button className="btn btn-outline-primary" onClick={this.add}>+</button> 
+                         <button className="btn btn-outline-primary" onClick={this.remove} disabled={this.state.qty<1}>-</button> 
+
+                         </div>
+                     </div>
+
+                 </div>
+                 <hr/>
+             </div>
+
+        );
+    }
+
+}
+class ProductList extends React.Component{
+    constructor (props){
+        super (props);
+        this.state={
+            productList:""
+         };
+    }
+    componentDidMount(){
+        setTimeout(()=>{
+
+            this.setState({productList : productList})
+        }, 1000);
+    }
+    render(){
+        if(!this.state.productList) return <p>Carregando....</p>
+
+        const Component = this;
+        const products = this.state.productList.map(
+            function(product){
+                return(
+                    <Product name={product.name}
+                    price ={product.price} info={product.info}/>
+            );
+            });
+
+            return(
+                <div>
+                     {products}
+                </div>
+               
+            );
+        
+
+    }
+}
+function App(){
+    return (
+        <div>
+            <ProductList/>
+        </div>
+    )
+
 }
 
 export default App;
